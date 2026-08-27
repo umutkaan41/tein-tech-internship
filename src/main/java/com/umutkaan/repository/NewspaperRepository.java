ddff6a2acaee5574e16b343de1827cc9dc8a4a15
Umut Kaan Özyürek <umutozyurek789@gmail.com>
2026-08-27 14:48:48 +0300

    Delete src directory

--- src/main/java/com/umutkaan/repository/NewspaperRepository.java
+++
@@ -1,8 +0,0 @@
-package com.umutkaan.repository;
-
-import com.umutkaan.entity.Newspaper;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewspaperRepository extends JpaRepository<Newspaper, Long> {
-
-}
--- src/main/java/com/umutkaan/repository/SportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.umutkaan.repository;
-import com.umutkaan.entity.Sport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SportRepository extends JpaRepository<Sport, Long> {
-
-
-}
